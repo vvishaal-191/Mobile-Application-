afee26dd16df78b4f4248727f6a2ae476ab8ca35
Vishaal <vvishaal191@gmail.com>
2026-09-22 10:36:43 +0530

    Remove active highlight from Profile icon on Holiday Calendar page

--- EmergereApp/EmergereApp/src/screens/HolidayCalendar/HolidayCalendarScreen.jsx
+++ EmergereApp/EmergereApp/src/screens/HolidayCalendar/HolidayCalendarScreen.jsx
@@ -97,7 +97,7 @@ export default function HolidayCalendarScreen({ navigation }) {
         })}
       </ScrollView>
 
-      <BottomNavBar active="Profile" onNavigate={go} />
+      <BottomNavBar onNavigate={go} />
     </View>
   );
 }
